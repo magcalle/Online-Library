ba1dbd7cc082e2a6da78039a8bb5c3c7347715fd
magcalle <43582904+magcalle@users.noreply.github.com>
2019-06-03 18:14:20 -0400

    Add files via upload

---
+++ myonlinelibrary/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Dirty Rocker Boys",
+	"img" => "img/media/bobbiebrown.png",
+    "genre" => "Biography",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Bobbie Brown", "Caroline Ryder"
+    ],
+    "publisher" => "Gallery Books, First Edition edition",
+    "isbn" => '978147673347005'
+];
+$catalog[102] = [
+    "title" => "Every Breath",
+    "img" => "img/media/everybreath.png",
+    "genre" => "Romance, Contemporary Romance, Domestic Fiction",
+    "format" => "Hardcover",
+    "year" => 2018,
+    "category" => "Books",
+    "authors" => [
+        "Nicholas Sparks"
+    ],
+    "publisher" => "Grand Central Publishing, First Edition edition",
+    "isbn" => '9781538715772'
+];
+$catalog[103] = [
+    "title" => "Giovanni's Room",
+    "img" => "img/media/room.png",
+    "genre" => "Novel, Psychological Fiction, Gay Literature",
+    "format" => "Paperback",
+    "year" => 1956,
+    "category" => "Books",
+    "authors" => [
+        "James Baldwin"
+    ],
+    "publisher" => "Dial Press, N.Y.",
+    "isbn" => '9781841339722'
+];
+$catalog[104] = [
+    "title" => "A sucky Love Story",
+    "img" => "img/media/suckylove.png",
+    "genre" => "True Crime",
+    "format" => "Hardcover",
+    "year" => 2018,
+    "category" => "Books",
+    "authors" => [
+        "Brittani Louise Taylor"
+    ],
+    "publisher" => "Post Hill Press",
+    "isbn" => '9781642930009'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Clueless",
+    "img" => "img/media/clueless.png",
+    "genre" => "Romantic Comedy",
+    "format" => "DVD/Streaming",
+    "year" => 1956,
+    "category" => "Movies",
+    "director" => "Amy Heckerling",
+    "writers" => [
+        "Amy Heckerling",
+        
+    ],
+    "stars" => [
+        "Alice SliverStone",
+        "Paul Rudd",
+        "Brittany Murphy",
+		 "Stacey Dash",
+    ]
+];
+$catalog[202] = [
+    "title" => "Resident Evil: Afterlife",
+    "img" => "img/media/evil.png",
+    "genre" => "Action, Horror",
+    "format" => "DVD/Streaming",
+    "year" => 2010,
+    "category" => "Movies",
+    "director" => "Paul W. S. Anderson",
+    "writers" => [
+        "Jonathan Nolan",
+				"Christopher Nolan"
+    ],
+    "stars" => [
+			"Milla Jovovich","Ali Larter","Kim Coates",
+			 "Shawn Roberts","Sergio Peris-Mencheta",
+		"Spencer Locke","Boris Kodjoe","Wentworth Miller",
+    ]
+];
+$catalog[203] = [
+    "title" => "Taken",
+    "img" => "img/media/Taken.png",
+    "genre" => "Action, Thriller",
+    "format" => "DVD",
+    "year" => 2000,
+    "category" => "Movies",
+    "director" => "Pierre Morel",
+    "writers" => [
+        "Luc Besson",
+        "Robert Mark Kamem"
+    ],
+    "stars" => [
+			"Maggie Grace",
+			"Lian Neeson",
+			"Katie Cassidy"
+    ]
+];
+$catalog[204] = [
+    "title" => "White Chicks",
+		"img" => "img/media/whitechicks.png",
+	  "genre" => "Action, Comedy",
+	  "format" => "DVD/Streaming",
+	  "year" => 2004,
+	  "category" => "Movies",
+	  "director" => "Keenen Ivory Wayans",
+	  "writers" => [
+	     "Keenen Ivory Wayans","Shawn Wayans",
+ "Marlon Wayans","Andrew McElfresh","Michael Anthony Snowden","Xavier Cook","Keenen Ivory Wayans","Shawn Wayans","Marlon Wayans"
+
+
+	  ],
+	  "stars" => [
+	     "Shawn Wayans","Marlon Wayans","Marcus Copeland","Jaime King","Frankie Faison", "Lochlyn Munro","John Heard", "Busy Philipps","Terry Crews","Brittany Daniel",	"Eddie Velez","Jessica Cauffiel", "Maitland Ward", "Anne Dudek","Rochelle Aytes","Jennifer Carpenter", "Faune Chambers Watkins","John Reardon", "Steven Grayhm", "Drew Sidora"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "X100pre",
+	"img" => "img/media/bunny.png",
+	"genre" => "Latin Trap",
+	"format" => "CD/Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Bad Bunny"
+];
+
+$catalog[302] = [
+	"title" => "Shawn Mendez",
+	"img" => "img/media/mendez.png",
+	"genre" => "Pop Music",
+	"format" => "Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Shawn Mendez"
+];
+$catalog[303] = [
+	"title" => "New Jersey",
+	"img" => "img/media/newjersey.png",
+	"genre" => "Hard Rock, Glam Metal",
+	"format" => "CD, Streaming Services",
+	"year" => 1988,
+	"category" => "Music",
+	"artist" => "Bon Jovi"
+];
+$catalog[304] = [
+	"title" => "Aura",
+	"img" => "img/media/ozuna.png",
+	"genre" => "Reggaton",
+	"format" => "CD, Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Ozuna"
+];
